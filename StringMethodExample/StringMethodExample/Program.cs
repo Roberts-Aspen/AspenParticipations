b225c43ed6187eb39f8a98c17690521aa6ca9935
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2019-09-17 11:11:48 -0500

    string method inclass

---
+++ StringMethodExample/StringMethodExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Aspen Roberts
+
+namespace StringMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your favorite word >>");
+            string answer = Console.ReadLine().ToUpper().Trim();
+            string anotherString = "Something else";
+            string answerASBIGLETTERS = answer.ToUpper();
+
+            for (int i = 0; i < answer.Length; i++)
+            {
+                Console.WriteLine(answerASBIGLETTERS[i] + " ");
+            }
+            
+            Console.WriteLine($"Your favorite word has {answer.Length} characters in it");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
