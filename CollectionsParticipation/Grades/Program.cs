bb9821ac5853bc0aa49d3586ca8f9008066d6188
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2019-09-26 11:34:06 -0500

    second q done

--- CollectionsParticipation/Grades/Program.cs
+++ CollectionsParticipation/Grades/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Aspen Roberts
 namespace Grades
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            List<Int32> gradeList = new List<Int32>();
+            string gradeAsString;
+            do
+            {
+                Console.WriteLine("Do you have a numerical grade to enter? If so enter it below, if not enter 'NO'");
+                gradeAsString = Console.ReadLine();
+
+                if (gradeAsString == "NO")
+                {
+                    double gradesAverage = gradeList.Average();
+                    Console.WriteLine($"The average of your grades is {gradesAverage}");
+                }
+                else
+                {
+                    int grade = Convert.ToInt32(gradeAsString);
+                    gradeList.Add(grade);
+                }
+
+            } while (gradeAsString != "NO");
+
+            Console.ReadKey();
         }
     }
 }
