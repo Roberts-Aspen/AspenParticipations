2fe3b2979bfbe3584136c9245a916a0f9dfe52d0
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2019-09-26 11:07:30 -0500

    first q done

---
+++ CollectionsParticipation/CollectionsParticipation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Aspen Roberts
+namespace CollectionsParticipation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter three course subjects seperated by a ',' (EX. MIS, MATH, etc.");
+            string userSubjects = Console.ReadLine();
+            string[] spieces = userSubjects.Split(',');
+
+            Console.WriteLine("Please enter three course numbers seperated by a ',' (EX. 3013, 3113, etc.");
+            string userNumbers = Console.ReadLine();
+            string[] npiecesAsString = userNumbers.Split(',');
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(spieces[i] + npiecesAsString[i]);
+            }
+            Console.ReadKey();
+            {
+
+            }
+        }
+    }
+}
