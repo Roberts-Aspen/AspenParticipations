55afb67d180e942cb034322a33a49ae23244a25e
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2019-10-02 19:36:56 -0500

    review questions

---
+++ MidtermReview/Accumulators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Aspen Roberts
+/*Write an application that will output all of the numbers from 1 to 100
+ * added together as well as output all of the numbers from 1 to 100 multiplied together.*/
+
+namespace Accumulators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 101; i++) //i++ means it adds one to i
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+
+            double multiply = 1;
+            for (int i = 1; i < 101; i++)
+            {
+                multiply = multiply * i;
+            }
+            Console.WriteLine(multiply);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MidtermReview/MidtermReview/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Aspen Roberts
+namespace MidtermReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What month is it?");
+            string month = Console.ReadLine();
+            int days = 0;
+
+            if (month == "january" || month == "march" || month == "may" || month == "july" || month == "august" || month == "october" || month == "december")
+            {
+                days = 31;
+            }
+            else if (month == "april" || month == "june" || month == "september" || month == "november")
+            {
+                days = 30;
+            }
+            else if (month == "february")
+            {
+                Console.WriteLine("Is it a leap year?");
+                string leapYear = Console.ReadLine();
+
+                if (leapYear == "yes")
+                {
+                    days = 29;
+                }
+                else
+                {
+                    days = 28;
+                }
+            }
+
+            Console.WriteLine(days);
+            Console.ReadKey();
+
+        }
+
+        
+    }
+
+}
