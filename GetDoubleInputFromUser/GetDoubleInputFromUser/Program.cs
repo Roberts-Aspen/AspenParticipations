4d3ee723296e6c593dad1929a434db974da7b240
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2019-10-15 11:12:59 -0500

    question done

---
+++ GetDoubleInputFromUser/GetDoubleInputFromUser/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Aspen Roberts
+/*Create a function and name it GetDoubleInputFromUser, that will have a return type of double. 
+ *Your function should accept 1 parameter, of type string, which will contain the message you want to ask the user. 
+Your function should ask the message passed in as an argument and get the users response.  
+You should validate that the response the user gave you is of type double (using double.TryParse ...), if it is not, you need to re-prompt them until they give you a valid input.  
+After you have a valid input from the user, return the converted value.
+Your main() function should call your GetDoubleInputFromUser function and multiply the users response by 5.
+Output the equation and result to the user (e.g. If the user entered the value 10 you would output, "10 * 5 = 50")
+*/
+namespace GetDoubleInputFromUser
+{
+    class Program
+    {
+        static double GetDoubleInputFromUser(string msg)
+        {
+            return 5;
+
+        }
+        static void Main(string[] args)
+        {
+            string sentence = "Please input a value.";
+            string userInput;
+            string validOrNot;
+            double newVal;
+
+            do
+            {
+                Console.WriteLine(sentence);
+                userInput = Console.ReadLine();
+
+                bool valid = double.TryParse(userInput, out newVal);
+
+                if (valid)
+                {
+                    validOrNot = "valid";
+                }
+                else
+                {
+                    validOrNot = "not";
+                }
+
+            } while (validOrNot == "not");
+
+            double calc = newVal * 5;
+            Console.WriteLine($"5 * {userInput} = {calc}");
+            Console.ReadKey();
+            
+        }
+    }
+}
