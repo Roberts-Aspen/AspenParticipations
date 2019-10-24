04579c36215cc7d5dcd1a21a849a2c53b149d3fb
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2019-10-24 11:32:08 -0500

    classes example

---
+++ ClassesExamples/ClassesExamples/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExamples
+{
+    class Student
+    {
+        private string name;
+
+        private int id;
+
+        public string city { get; set; }
+
+        public int getID()
+        {
+            return id;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setID(int newID)
+        {
+            id = newID;
+        }
+       
+        public Student()
+        {
+            id = 0;
+            name = string.Empty; //"
+        }
+
+    }
+}
---
+++ ClassesExamples/ClassesExamples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Aspen";
+
+            Student myStudent = new Student();
+
+            myStudent.setName(myName);
+
+            myStudent.setID(5);
+
+            Console.WriteLine(myStudent.getName() + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
