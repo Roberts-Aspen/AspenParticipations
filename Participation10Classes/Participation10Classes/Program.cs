dba154b8c62adbba548dba05df4859b090728559
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2019-10-29 10:56:55 -0500

    rectangle part done

---
+++ Participation10Classes/Participation10Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Aspen Roberts
+namespace Participation10Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            rect1.Length = 5;
+            rect1.Width = 10;
+            double p1 = rect1.CalculatePerimeter();
+
+            Console.WriteLine($"Rectangle 1 with {rect1.Length} length and {rect1.Width} width has a perimeter of {p1} and an area of {rect1.CalculateArea()}");
+
+            Rectangle rect2 = new Rectangle();
+            rect2.Length = 200;
+            rect2.Width = 5;
+
+            Console.WriteLine($"Rectangle 2 with {rect2.Length} length and {rect2.Width} width has a perimeter of {rect2.CalculatePerimeter()} and an area of {rect2.CalculateArea()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participation10Classes/Participation10Classes/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation10Classes
+{
+    class Rectangle
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+
+        }
+
+        public double CalculateArea()
+        {
+            double area = Length * Width;
+            return area;
+        }
+
+        /// <summary>
+        /// Calculates the perimeter of the rectangle
+        /// </summary>
+        /// <returns>
+        /// A double that is the perimeter value
+        /// </returns>
+
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+    }
+
+
+
+}
