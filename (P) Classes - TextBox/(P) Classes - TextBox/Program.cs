2c49b71181f54befda1d0957d38cac5dfa431642
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2019-11-05 11:41:18 -0600

    as much done as i could

---
+++ (P) Classes - TextBox/(P) Classes - TextBox/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Aspen Roberts
+namespace _P__Classes___TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox tb = new TextBox();
+            TextBox tb1 = new TextBox();
+            TextBox tb2 = new TextBox();
+
+            tb1.Padding = 10;
+            tb1.BackColor = ConsoleColor.DarkGray;
+            tb1.ForeColor = ConsoleColor.DarkYellow;
+            tb1.Text = "My Name is Aspen";
+
+            tb2.Padding = 5;
+            tb2.BackColor = ConsoleColor.DarkRed;
+            tb2.ForeColor = ConsoleColor.White;
+            tb2.Text = "I love MIS";
+
+            tb1.DisplayText();
+            tb2.DisplayText();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ (P) Classes - TextBox/(P) Classes - TextBox/TextBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__Classes___TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+
+        public ConsoleColor BackColor {get; set;}
+
+        public ConsoleColor ForeColor { get; set;}
+
+        public string Text { get; set; }
+
+        public TextBox()
+        {
+            Padding = 0;
+            BackColor = ConsoleColor.Blue;
+            ForeColor = ConsoleColor.Black;
+            Text = "";
+        }
+
+        public void DisplayText()
+        {
+            Console.BackgroundColor = BackColor;
+
+            Console.ForegroundColor = ForeColor;
+
+            Console.WriteLine(Text);
+
+            for (int i = 0; i < Padding - 1; i++)
+            {
+                
+            }
+
+            Console.WriteLine($"{Padding}{Text}{Padding}");
+        }
+        
+
+    }
+}
