edc030a5b15528cb176715253bcf690a5754e096
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2020-01-30 13:07:39 -0600

    wpf done

---
+++ WPF1/WPF1/EntryForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public EntryForm()
+        {
+            string Name = "";
+            string Address = "";
+            int ZipCode = 0;
+
+        }
+
+        public EntryForm(string name, string address, int zipCode)
+        {
+            name = Name;
+            address = Address;
+            zipCode = ZipCode;
+        }
+        public override string ToString()
+        {
+            string over = Name + " " + Address + " " + ZipCode;
+
+            return over;
+        }
+    }
+}
---
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//Aspen Roberts
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txtName.Text = "";
+            txtAddress.Text = string.Empty;
+            txtZip.Text = string.Empty;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string Name, Address, ZipCode;
+
+            Name = txtName.Text;
+            Address = txtAddress.Text;
+            ZipCode = txtZip.Text;
+
+            //EntryForm entry = new EntryForm(Name, Address, Convert.ToInt32(ZipCode));
+
+            listBox.Items.Add($"{Name} {Address} {ZipCode}");
+
+          
+        }
+    }
+}
