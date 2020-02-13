29157f3bbee5de32aaeb17ff908ab0c7dcef6031
Aspen Roberts <Aspen.N.Roberts-1@ou.edu>
2020-02-13 12:40:48 -0600

    started

---
+++ (P) JSON - Pokemon/(P) JSON - Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//Aspen Roberts
+namespace _P__JSON___Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string path = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000";
+
+            PokeAPI PokemonAPI;
+
+            using (var client = new HttpClient())
+            {
+
+                string json = client.GetStringAsync(path).Result;
+
+                PokemonAPI = JsonConvert.DeserializeObject<PokeAPI>(json);
+            }
+
+            foreach (var character in PokemonAPI.results)
+            {
+                Characters.Items.Add(character);
+            }
+
+        }
+
+        private void LstCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCharacterFromList = (ResultObject)Characters.SelectedItem;
+
+            //imgCharacter.Source = new BitmapImage(new Uri("https://pokeapi.co/api/v2/pokemon/pikachu"));
+            imgCharacter.Source = new BitmapImage(new Uri(selectedCharacterFromList.image));
+        }
+
+    }
+}
---
+++ (P) JSON - Pokemon/(P) JSON - Pokemon/PokeAPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Aspen Roberts
+namespace _P__JSON___Pokemon
+{
+    class PokeAPI
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+        public List<PokeResult> results { get; set; }
+
+    }
+
+    public class PokeResult
+    {
+        public string name { get; set; }
+
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+    }
+
+    public class ResultObject
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+
+    }
+}
+
